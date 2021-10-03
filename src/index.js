17e2284654b13f55acd5914db7061c2cec9d1a6f
Edward Smith <edward.m.smith80@gmail.com>
2021-10-03 17:10:38 -0600

    reset starter code

--- src/index.js
+++ src/index.js
@@ -1,11 +1,11 @@
-import React, { useState } from 'react';
+import React, { useState, Fragment } from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 
 function Application() {
 
   // your code here
-  
+
   function reset() {
     // your code here
   }
@@ -16,8 +16,8 @@ function Application() {
 
   return (
     <main>
-      {/* your code here -- this entire line and curly braces should be removed */}
-      <h1>Hello</h1>
+      {/* your code here -- this entire line including the curly braces can be removed */}
+      <h1>Hello, React</h1>
     </main>
   );
 }
